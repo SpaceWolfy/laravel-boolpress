9622bc2e347f1d73ae7add7315c11fe53bf3c3fd
Samuel Scozzari <s_scozzari@libero.it>
2022-03-14 16:54:55 +0100

    adds posts index structure

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,7 +6,21 @@
 
 
 @section('content')
-<div class="page-container">
- 
+<div class="my-posts-container">
+ <div class="my-posts-section">
+    <div class="my-top-nav">
+      <h2>Gestisci i post</h2>
+      <a class="" href="{{ route('admin.posts.create') }}">Aggiungi un post</a>
+    </div>
+
+
+      @foreach ($dataPosts as $post)
+        <div class="my-div">{{$post->postTitle}}
+          <a href="{{ route('admin.posts.show', $post->slug) }}">Mostra</a>
+        </div>
+        <hr class="my-line">
+      @endforeach
+
+ </div>
 </div>
 @endsection
