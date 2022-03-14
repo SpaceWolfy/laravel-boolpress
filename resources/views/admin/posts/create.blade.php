3d00f698480417620c36740790442c8177ad62b8
Samuel Scozzari <s_scozzari@libero.it>
2022-03-14 16:53:54 +0100

    Adds Post's create files, style & setup

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,7 +6,40 @@
 
 
 @section('content')
-<div class="page-container">
+<div class="my-form-cont">
+  <div><h1>Aggiungi un nuovo post:</h1></div>
   
+
+ <form action="{{ route('admin.posts.store') }}" method="post">
+  @csrf
+
+  {{-- Titolo del post --}}
+  <div class="my-title">
+    <label>Titolo del post</label>
+    
+    <input type="text" name="postTitle" class="@error('postTitle') is-invalid @enderror" placeholder="Inserisci il titolo del post" value="{{old('postTitle')}}">
+    
+    @error('postTitle')
+      <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+  </div>
+
+  {{-- Testo del post --}}
+  <div class="my-text">
+    <label>Contenuto del post</label>
+
+    <textarea name="postText" cols="30" rows="15" class="@error('postText') is-invalid @enderror"
+    placeholder="Inserisci qui il contenuto del tuo post..." required>{{ old('postText') }}</textarea>
+
+    @error('postText')
+      <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+  </div>
+
+  <div class="my-form-group">
+    <button type="submit" class="my-btn-save">Salva il post</button>
+    <a href="{{ route('admin.posts.index') }}" class="">Torna indietro</a>
+  </div>
+</form>
 </div>
 @endsection
