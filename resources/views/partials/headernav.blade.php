e6cb17cb5e6493602c8c4e957c0bc8be06f1e1de
Samuel Scozzari <s_scozzari@libero.it>
2022-03-14 15:12:01 +0100

    adds a new voice in nav

--- resources/views/partials/headernav.blade.php
+++ resources/views/partials/headernav.blade.php
@@ -1,6 +1,7 @@
 <nav class="navigation-bar">
   <ul class="my-ul">
     <li class="nav-link {{ Request::route()->getName() === 'admin.home' ? 'active' : '' }}"><a href="{{ route('admin.home') }}">HOME</a></li>
+    <li class="nav-link {{ Request::route()->getName() === 'admin.posts.index' ? 'active' : '' }}"><a href="{{ route('admin.posts.index') }}">POSTS</a></li>
     <li class="nav-link {{ Request::route()->getName() === 'login-page' ? 'active' : '' }}"><a href="{{ route('login-page') }}">ACCOUNT</a></li>
   </ul>
 </nav>
