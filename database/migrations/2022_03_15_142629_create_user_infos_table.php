08b94c238d79a1e08ec413d9b3529f6f2f71ede4
Samuel Scozzari <s_scozzari@libero.it>
2022-03-15 15:52:00 +0100

    adds UserInfo model & migration

---
+++ database/migrations/2022_03_15_142629_create_user_infos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('phoneNumber')->nullable();
+            $table->string('address')->nullable();
+            $table->string('proPic')->nullable();
+
+            $table->foreignId('users_id')
+                ->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+}
