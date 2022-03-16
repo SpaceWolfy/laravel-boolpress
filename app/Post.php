53192c99806d06266c71075bc925daa209dc6f3e
Samuel Scozzari <s_scozzari@libero.it>
2022-03-16 13:57:45 +0100

    adds new function of relation

--- app/Post.php
+++ app/Post.php
@@ -17,4 +17,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\Category');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
