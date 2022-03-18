1f6240196ccceda833a6a1a0379c1c4db9871b51
Samuel Scozzari <s_scozzari@libero.it>
2022-03-18 16:56:07 +0100

    Adds navbar component

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-lg navigation-bar">
+        <a class="navbar-brand" href="/login">Boolpress</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.navbar-brand {
+    font-size: 25px;
+}
+</style>
