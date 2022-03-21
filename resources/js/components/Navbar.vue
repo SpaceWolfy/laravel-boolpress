0f872b42ab2219c3b57cadbfd8ecbca8b1f572b2
Samuel Scozzari <s_scozzari@libero.it>
2022-03-21 15:53:36 +0100

    creates router-link section

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navigation-bar">
         <a class="navbar-brand" href="/login">Boolpress</a>
+
         <button
             class="navbar-toggler"
             type="button"
@@ -12,11 +13,35 @@
         >
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li
+                    class="nav-item active"
+                    v-for="route in routesArray"
+                    :key="route.path"
+                >
+                    <router-link class="nav-link" :to="route.path">{{
+                        route.meta.linkText
+                    }}</router-link>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            routesArray: [],
+        };
+    },
+    mounted() {
+        this.routesArray = this.$router
+            .getRoutes()
+            .filter((route) => !!route.meta.linkText);
+    },
+};
 </script>
 
 <style>
